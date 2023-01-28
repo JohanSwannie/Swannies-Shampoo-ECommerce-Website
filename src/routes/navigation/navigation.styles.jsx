95d15ec02969d7783c5e26069b707a1ca8720fff
jswan <jswannieman@gmail.com>
2023-01-28 17:07:30 +1100

    Navigation Styled Component Updated

--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const NavigationContainer = styled.div`
-  height: 70px;
+  height: 120px;
   width: 100%;
   display: flex;
   justify-content: space-between;
-  background: rgba(236, 225, 205, 0.5);
+  background: rgba(236, 225, 205, 0.9);
+  margin: 0 0 10px 0;
 `;
 
 export const LogoContainer = styled(Link)`
@@ -16,8 +17,9 @@ export const LogoContainer = styled(Link)`
 `;
 
 export const Image = styled.img`
-  width: 200px;
-  height: 90px;
+  width: 160px;
+  height: 80px;
+  margin-top: 0;
   mix-blend-mode: multiply;
 `;
 
@@ -26,12 +28,16 @@ export const BusinessName = styled.div`
   font-size: 40px;
   font-weight: lighter;
   padding: 10px 15px;
+  margin-left: 1rem;
+  line-height: 2;
   opacity: 0.9;
 `;
 
 export const NavLinks = styled.div`
-  width: 50%;
+  font-family: "Stint Ultra Condensed", cursive;
+  width: 40%;
   height: 100%;
+  font-size: 30px;
   display: flex;
   align-items: center;
   justify-content: flex-end;
