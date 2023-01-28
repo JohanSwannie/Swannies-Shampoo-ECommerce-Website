c08eb0177ed9336587a3fcdac5e290af104f4165
jswan <jswannieman@gmail.com>
2023-01-28 14:44:50 +1100

    Updates to Navigation Styled Component

--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -1,18 +1,32 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const NavigationContainer = styled.div`
   height: 70px;
   width: 100%;
   display: flex;
   justify-content: space-between;
-  margin-bottom: 25px;
+  background: rgba(236, 225, 205, 0.5);
 `;
 
 export const LogoContainer = styled(Link)`
   height: 100%;
-  width: 70px;
+  width: 160px;
   padding: 25px;
+`;
+
+export const Image = styled.img`
+  width: 200px;
+  height: 90px;
+  mix-blend-mode: multiply;
+`;
+
+export const BusinessName = styled.div`
+  font-family: "Stint Ultra Condensed", cursive;
+  font-size: 40px;
+  font-weight: lighter;
+  padding: 10px 15px;
+  opacity: 0.9;
 `;
 
 export const NavLinks = styled.div`
