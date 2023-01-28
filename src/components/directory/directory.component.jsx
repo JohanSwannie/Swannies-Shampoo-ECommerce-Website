6a067d3c4ae188ae76b0398335587b681240651c
jswan <jswannieman@gmail.com>
2023-01-28 17:06:14 +1100

    Directory Component updated

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,37 +1,34 @@
-import DirectoryItem from '../directory-item/directory-item.component';
+import DirectoryItem from "../directory-item/directory-item.component";
 
-import { DirectoryContainer } from './directory.styles';
+import { DirectoryContainer } from "./directory.styles";
 
 const categories = [
   {
     id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    route: 'shop/hats',
+    title: "Men's Shampoo",
+    imageUrl:
+      "https://img.freepik.com/premium-photo/two-brown-beige-plastic-shampoo-bottles-floating-studio-background-3d-render_723234-1011.jpg",
+    route: "shop/men's shampoo",
   },
   {
     id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    route: 'shop/jackets',
+    title: "Women's Shampoo",
+    imageUrl: "https://m.media-amazon.com/images/I/41Cxpmgs3JL._AC_SY350_.jpg",
+    route: "shop/women's shampoo",
   },
   {
     id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    route: 'shop/sneakers',
+    title: "Children's Shampoo",
+    imageUrl:
+      "https://i.kym-cdn.com/news_feeds/icons/mobile/000/040/263/788.jpg",
+    route: "shop/children's shampoo",
   },
   {
     id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    route: 'shop/womens',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    route: 'shop/mens',
+    title: "Hair Conditioners",
+    imageUrl:
+      "https://images.askmen.com/1080x540/2021/02/05-013537-best_beard_conditioners.jpg",
+    route: "shop/hair conditioners",
   },
 ];
 
