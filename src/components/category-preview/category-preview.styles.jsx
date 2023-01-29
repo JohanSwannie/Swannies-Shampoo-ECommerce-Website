ca8e7924af59cd2075d1a960f01a7901e6422c41
jswan <jswannieman@gmail.com>
2023-01-29 14:42:49 +1100

    Update the Category-preview Styled Component

--- src/components/category-preview/category-preview.styles.jsx
+++ src/components/category-preview/category-preview.styles.jsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const CategoryPreviewContainer = styled.div`
   display: flex;
@@ -10,8 +10,15 @@ export const CategoryPreviewContainer = styled.div`
 
 export const Title = styled(Link)`
   font-size: 28px;
+  text-shadow: 1px 1px 2px red, 0 0 1em orange, 0 0 0.2rem orange;
   margin-bottom: 25px;
+  border-radius: 20%;
+  padding: 0.5rem;
   cursor: pointer;
+  &:hover {
+    background-color: darkgray;
+    color: #fff;
+  }
 `;
 
 export const Preview = styled.div`
