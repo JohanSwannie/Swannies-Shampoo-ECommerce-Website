1751525c08468f3c090f9f80f9e7c46d4a775191
jswan <jswannieman@gmail.com>
2023-01-29 11:48:53 +1100

    Update Index Component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./store/store";
-
+import App from "./App";
 import "./index.scss";
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
