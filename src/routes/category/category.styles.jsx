bcdabe12507e15df0ee93a0d5ba429705607cc3d
jswan <jswannieman@gmail.com>
2023-01-29 14:43:42 +1100

    Update the Category Styled Component

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CategoryContainer = styled.div`
   display: grid;
@@ -11,4 +11,6 @@ export const Title = styled.h2`
   font-size: 38px;
   margin-bottom: 25px;
   text-align: center;
+  text-shadow: 1px 1px 2px red, 0 0 1em orange, 0 0 0.2rem orange;
+  cursor: pointer;
 `;
