096a69a937047c5f63793a2e2b17c5558bdad62e
jswan <jswannieman@gmail.com>
2023-01-29 13:42:07 +1100

    Update Category Component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect, Fragment } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect, Fragment, useLayoutEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
-import { CategoryContainer, Title } from './category.styles';
+import { CategoryContainer, Title } from "./category.styles";
 
 const Category = () => {
   const { category } = useParams();
@@ -16,6 +16,10 @@ const Category = () => {
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  });
 
   return (
     <Fragment>
