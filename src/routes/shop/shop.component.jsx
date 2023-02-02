91b3ae55d8c4bbcaf8225cb6c8e85efdcf356ed3
jswan <jswannieman@gmail.com>
2023-02-02 11:52:33 +1100

    Update Shop JavaScript Component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,35 +1,24 @@
-import { useEffect } from "react";
-
-import { Routes, Route } from "react-router-dom";
-
-import { useDispatch } from "react-redux";
-
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-
-import { setCategories } from "../../store/categories/category.action";
-
-import CategoriesPreview from "../categories-preview/categories-preview.component";
-
-import Category from "../category/category.component";
-
 // import SHOP_DATA from "../../shop-data.js";
 
 // import { addCollectionAndDocuments } from "../../utils/firebase/firebase.utils";
 
+// useEffect(() => {
+//   addCollectionAndDocuments("categories", SHOP_DATA, "title");
+// }, []);
+
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+import { fetchCategoriesStartAsync } from "../../store/categories/category.action";
+
 const Shop = () => {
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   addCollectionAndDocuments("categories", SHOP_DATA, "title");
-  // }, []);
-
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments("categories");
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategoriesMap();
+    dispatch(fetchCategoriesStartAsync());
   }, [dispatch]);
 
   return (
