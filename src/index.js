46795f2a264a2ae1a4ba51ce92d292b32831ca37
jswan <jswannieman@gmail.com>
2023-02-07 09:25:26 +1100

    Update Index JavaScript Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./store/redux-store";
+import { BrowserRouter } from "react-router-dom";
 import { Elements } from "@stripe/react-stripe-js";
+import { store, persistor } from "./store/redux-store";
 import { stripePromise } from "./utils/stripe/stripe.utils";
 import App from "./App";
 import "./index.scss";
