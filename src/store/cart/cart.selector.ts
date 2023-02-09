c0cfc74af70ba14bca1d6052a6736f5b391b760d
jswan <jswannieman@gmail.com>
2023-02-09 12:57:08 +1100

    Update cart-selector TS Component

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -1,7 +1,9 @@
 import { createSelector } from 'reselect';
-import { CartState } from './cart.reducer';
 
-const selectCartReducer = (state): CartState => state.cart;
+import { CartState } from './cart.reducer';
+import { RootState } from '../redux-store';
+
+const selectCartReducer = (state: RootState): CartState => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCartReducer],
