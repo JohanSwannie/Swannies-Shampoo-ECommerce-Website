668360f6a72bf24947f69457fe0f8e86a57c7edc
jswan <jswannieman@gmail.com>
2023-02-10 19:48:07 +1100

    Update TSX Components

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -25,12 +25,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyDE20a3OFKpIUF8zJ8n-6hrBIbp1iNXMvY",
+  authDomain: "swannie-clothing-db.firebaseapp.com",
+  projectId: "swannie-clothing-db",
+  storageBucket: "swannie-clothing-db.appspot.com",
+  messagingSenderId: "575043720201",
+  appId: "1:575043720201:web:a923dfb1b26cd4f9a994d8"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
