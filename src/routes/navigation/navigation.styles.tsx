021630b037d845ce673580c648c96d748d798208
jswan <jswannieman@gmail.com>
2023-02-15 13:59:29 +1100

    Update navigation.styles TSX Component

--- src/routes/navigation/navigation.styles.tsx
+++ src/routes/navigation/navigation.styles.tsx
@@ -12,10 +12,22 @@ export const NavigationContainer = styled.div`
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
-  padding: 25px;
+  padding: 10px 20px;
+  margin-top: 0;
+`;
+
+export const BusinessName = styled.div`
+  font-family: "Stint Ultra Condensed", cursive;
+  font-size: 40px;
+  font-weight: lighter;
+  padding: 10px 15px;
+  opacity: 0.9;
 `;
 
 export const NavLinks = styled.div`
+  font-family: "Stint Ultra Condensed", cursive;
+  font-size: 26px;
+  font-weight: bold;
   width: 50%;
   height: 100%;
   display: flex;
