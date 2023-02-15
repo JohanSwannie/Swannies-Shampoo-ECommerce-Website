5176e7a7f2e0f2d921949369eb7fd129f0c5880c
jswan <jswannieman@gmail.com>
2023-02-15 13:58:25 +1100

    Update authentication.component TSX Component

--- src/routes/authentication/authentication.component.tsx
+++ src/routes/authentication/authentication.component.tsx
@@ -1,32 +1,15 @@
-import { Fragment } from 'react';
-import { useSelector } from 'react-redux';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
-import {
-  selectCategoriesMap,
-  selectCategoriesIsLoading,
-} from '../../store/categories/category.selector';
+import { AuthenticationContainer } from './authentication.styles';
 
-import CategoryPreview from '../../components/category-preview/category-preview.component';
-import Spinner from '../../components/spinner/spinner.component';
-
-const CategoriesPreview = () => {
-  const categoriesMap = useSelector(selectCategoriesMap);
-  const isLoading = useSelector(selectCategoriesIsLoading);
-
+const Authentication = () => {
   return (
-    <Fragment>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        Object.keys(categoriesMap).map((title) => {
-          const products = categoriesMap[title];
-          return (
-            <CategoryPreview key={title} title={title} products={products} />
-          );
-        })
-      )}
-    </Fragment>
+    <AuthenticationContainer>
+      <SignInForm />
+      <SignUpForm />
+    </AuthenticationContainer>
   );
 };
 
-export default CategoriesPreview;
+export default Authentication;
